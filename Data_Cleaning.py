8d59357b42dbf8b87fda2728d9f19549790b1a69
Shubham <sjais789@users.noreply.github.com>
2018-07-08 13:45:59 +0200

    Create Data_Cleaning.py

---
+++ Data_Cleaning.py
@@ -0,0 +1,15 @@
+import pandas as pd
+# data frame 
+p= pd.read_csv('file_name')
+# head ,tail method and shape,column attribute
+print(p.head())
+print(p.tail())
+print(p.shape)
+print(p.columns)
+
+print (p.info())
+
+# Print the info of df_subset
+
+print(p_subset.info())
+
